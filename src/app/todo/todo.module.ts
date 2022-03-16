1aef5881721ce9b120500bfb3cc50c433f231901
j.untch <j.untch@outlook.de>
2022-03-16 21:28:04 +0100

    implementation task-item component base ui

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskAddComponent } from './task-add/task-add.component';
+import { TaskItemComponent } from './task-item/task-item.component';
+
+const components = [TaskAddComponent, TaskItemComponent];
 
 @NgModule({
-  declarations: [TaskAddComponent],
+  declarations: components,
   imports: [CommonModule],
-  exports: [TaskAddComponent],
+  exports: components,
 })
 export class TodoModule {}
